f11f332fac253134c3c4e3ce6a529b16de110d60
Daniel8529 <carlosdanielmonterogomez@gmail.com>
2023-12-28 23:44:13 -0400

    PSP-28-Sign-In-UI-and-Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Route, Routes } from 'react-router-dom'
 import Calander from "./components/Calander/Calander"
 import Hours from './components/Hours/Hours'
+import Signln from './Layout/Signln'
 
 function App() {
 
@@ -8,7 +9,8 @@ function App() {
     <>
       <Routes>
         <Route>
-          <Route path="" element={<Calander />} />
+          {/* <Route path="" element={<Calander />} /> */}
+          <Route path="" element={<Signln />} />
           <Route path="/hours/:Dia/:Month/:Year" element={<Hours />} />
         </Route>
       </Routes>
--- src/Context.jsx
+++ src/Context.jsx
@@ -1,10 +1,10 @@
 import { createContext, useContext, useState, useEffect } from "react"
-import { db } from "./firebase/firebase"
+import { db,auth } from "./firebase/firebase"
 import { set, ref, onValue, get, update, push } from "firebase/database"
-
-
+import { signInWithEmailAndPassword,createUserWithEmailAndPassword, signOut } from "firebase/auth"
+import { toast } from "react-toastify"
 const Context = createContext()
-
+//123456789
 export const useContextAir = () => {
     const context  = useContext(Context)
     if(!context) throw new Error('There is no Context provider')
@@ -51,7 +51,30 @@ export function ProviderContext({children}) {
       }, [ReservationsForDate])
       
       
-
+      const signup = async (email, password) => {
+        // Validar el formato del correo electrónico
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+          console.error("Error: El formato del correo electrónico no es válido");
+          return;
+        }
+      
+        try {
+          await createUserWithEmailAndPassword(auth, email, password);
+          console.log("Usuario registrado exitosamente");
+        } catch (error) {
+          // Manejar errores específicos
+          if (error.code === "auth/weak-password") {
+            console.error("Error: La contraseña es débil. Debe tener al menos 6 caracteres");
+          } else if (error.code === "auth/email-already-in-use") {
+            console.error("Error: El correo electrónico ya está en uso. Prueba con otro");
+          } else if (error.code === "auth/invalid-email") {
+            console.error("Error: El formato del correo electrónico no es válido");
+          } else {
+            console.error("Error al registrar usuario:", error.message);
+          }
+        }
+      }
 
       
     return (
@@ -59,7 +82,9 @@ export function ProviderContext({children}) {
             value={{
                 ShowListHours,
                 ReservationsForDate,
-                SaveScheduledform
+                SaveScheduledform,
+                signup,
+                useContextAir
             }}
         >
             {children}
