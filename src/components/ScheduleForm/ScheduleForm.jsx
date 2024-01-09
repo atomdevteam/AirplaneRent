405144f0b74ab953e8bd78d51b17d9f3c8d4874c
jose22gonzalez <urenajosealbeto@gmail.com>
2024-01-09 10:22:17 -0400

    .

--- src/components/ScheduleForm/ScheduleForm.jsx
+++ src/components/ScheduleForm/ScheduleForm.jsx
@@ -248,7 +248,6 @@ const ScheduleForm = ({ isOpen, setIsOpen, onSave, reservations, setReservations
                                             name="name"
                                             placeholder="Name"
                                             value={name}
-                                            disabled={true}
                                             onChange={(e) => setname(e.target.value)}
                                             className="py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 shadow-sm" />
                                     </div>
