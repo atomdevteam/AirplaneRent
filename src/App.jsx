0239467e9e43de8610c26a29bd3beee175e60e73
Sander Peguero <sanderpeguero.21@gmail.com>
2024-01-15 05:12:50 -0400

    UI/UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Route, Routes } from 'react-router-dom'
 import Calander from "./components/Calander/Calander"
 import Hours from './components/Hours/Hours'
+import AdminDash from './layout/AdminDash'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route>
           <Route path="" element={<Calander />} />
+          <Route path="/admindash" element={<AdminDash/>} />
           <Route path="/hours/:Dia/:Month/:Year" element={<Hours />} />
         </Route>
       </Routes>
