7917a95cb834f6813e61ee9a0bddab629a4fbe43
Daniel8529 <carlosdanielmonterogomez@gmail.com>
2024-01-15 20:20:38 -0400

    PSP-54-Block-4-Owner-Reserve-Details-Account-Profile

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,32 @@ import LogIn from './components/LogIn/LogIn';
 import { useContextAir } from './Context';
 import Signln from './Layout/Signln';
 import Loader from './components/Loader/Loader';
+import ReserveDetails from './components/ReserveDetails/ReserveDetails';
 
 function App() {
-  const { user } = useContextAir();
-  const userisAuth = localStorage.getItem("Token")
+  // const { user } = useContextAir();
+  // const userisAuth = localStorage.getItem("Token")
 
-  if (userisAuth) {
-    return (
-      <Routes>
-        <Route path="/" element={<Calander />} />
-        <Route path="/calender" element={<Calander />} />
-        <Route path="/hours/:Dia/:Month/:Year" element={<Hours />} />
-      </Routes>
-    );
-  } else {
-    return (
-      <Routes>
-        <Route path="/" element={<LogIn />} />
-        <Route path="/Signln" element={<Signln />} />
-      </Routes>
-    );
-  }
+  // if (userisAuth) {
+  //   return (
+  //     <Routes>
+  //       <Route path="/" element={<Calander />} />
+  //       <Route path="/calender" element={<Calander />} />
+  //       <Route path="/hours/:Dia/:Month/:Year" element={<Hours />} />
+  //     </Routes>
+  //   );
+  // } else {
+  //   return (
+  //     <Routes>
+  //       <Route path="/" element={<LogIn />} />
+  //       <Route path="/Signln" element={<Signln />} />
+  //     </Routes>
+  //   );
+  // }
+
+  return(
+    <ReserveDetails></ReserveDetails>
+  )
 }
 
 export default App;
