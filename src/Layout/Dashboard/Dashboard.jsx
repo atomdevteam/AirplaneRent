6d347c8426c916924b7a6f7ecb17903b6522bf66
jose22gonzalez <urenajosealbeto@gmail.com>
2024-01-16 15:09:59 -0400

    PSP-56 Aircraft Details

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import UserProfile from '../../components/Account/UserProfile'
 import AircraftReserves from '../../components/AircraftReserves/AircraftReserves'
+import AircraftDetailsForm from '../../components/AircraftDetailsForm/AircraftDetailsForm'
 const Dashboard = () => {
   return (
     <div className='flex flex-col'>
-        <UserProfile />
-        <AircraftReserves />
+      <AircraftDetailsForm />
+      {/* <UserProfile />
+        <AircraftReserves /> */}
     </div>
   )
 }
