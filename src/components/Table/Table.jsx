06a1ccafc76219a8087e3352892481fa52f32226
jose22gonzalez <urenajosealbeto@gmail.com>
2024-01-22 15:05:45 -0400

    PSP-72 User Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -129,7 +129,7 @@ function Table() {
     const handleClickNextPage = () => {
         setPage((prevPage) => Math.min(prevPage + 1, Math.ceil(arregloDatos.length / itemsPerPage)));
     };
-    
+
     const handleClickPrevPage = () => {
         setPage((prevPage) => Math.max(prevPage - 1, 1)); // No permitir ir a una página menor a 1
     };
@@ -139,85 +139,80 @@ function Table() {
         currentPage * itemsPerPage
     );
     const totalPages = Math.ceil(arregloDatos.length / itemsPerPage);
-const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+    const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
 
-const handlePageClick = (page) => {
-    setPage(page);
-};
-   
+    const handlePageClick = (page) => {
+        setPage(page);
+    };
+
 
     return (
         <>
-        <Navbar></Navbar>
-        <div className='bg-black h-creen'>
-        <h1 className="text-4xl pt-6 font-bold text-white mb-4">Aircrafts</h1>
-        <h6 className="text-1xl  font-bold border-b-2 border-b-white text-white mb-4">5 Aircrafts</h6>
-            <table className="min-w-full bg-black border border-gray-300 shadow-md rounded-md overflow-hidden">
-                <tbody>
-                    {paginatedData.map((item, index) => (
-                     <tr key={index} className='border-b-2 border-b-white  '>
-                     <td className="py-2 px-4">
-                         <div className="flex items-center">
-                             <img
-                                 src={item.imagen}
-                                 alt="Imagen de la persona"
-                                 className="w-10 h-10 rounded-full mr-2"
-                             />
-                             <div>
-                                 <p className="mb-1 pt-4 text-white text-sm">{item.nombre} </p>
-                                 <p className="text-xs text-white">{item.descripcion}</p>
-                             </div>
-                         </div>
-                     </td>
-                     <td className="py-2 px-4 text-xs flex justify-between"> {/* Añadida la clase flex y justify-between */}
-                         <div>
-                             <p className="mb-1 text-sm text-white">{item.pais} </p>
-                             <p className="text-xs text-white">{item.hora}</p>
-                         </div>
-                         <div>
-                             <button className="bg-orange-500 rounded-full px-4 py-2 text-white">
-                                 Detalles
-                             </button>
-                         </div>
-                     </td>
-                 </tr>
-                 
-                    ))}
-                </tbody>
-            </table>
+            <Navbar></Navbar>
+            <div className='bg-black h-creen mx-8 mb-12'>
+                <h1 className="text-2xl pt-6  text-white mb-4">Aircrafts</h1>
+                <div className='flex flex-row justify-between'>
+                    <h6 className="text-1xl text-gray-300 mb-4">5 Aircrafts</h6>
+                    <div className='text-gray-300 flex items-center mr-8'>Filter</div>
+                </div>
+                <div className='border-b-2 border-b-gray-700'></div>
+                <table className="min-w-full bg-black border border-b-gray-700 shadow-md rounded-md overflow-hidden">
+                    <tbody>
+                        {paginatedData.map((item, index) => (
+                            <tr key={index} className='border-b-2 border-b-gray-700'>
+                                <td className="py-2 px-4">
+                                    <div className="flex items-center ">
+                                        <img
+                                            src={item.imagen}
+                                            alt="Imagen de la persona"
+                                            className="w-10 h-10 rounded-full mr-2"
+                                        />
+                                        <div className=''>
+                                            <p className="mb-1 pt-2 text-white text-sm">{item.nombre} </p>
+                                            <p className="text-xs text-gray-400">{item.descripcion}</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="py-2 px-4 text-xs flex justify-between"> {/* Añadida la clase flex y justify-between */}
+                                    <div className='mb-4 mt-4'>
+                                        <p className="mb-1 text-sm text-white">{item.pais} </p>
+                                        <p className="text-xs text-gray-400">{item.hora}</p>
+                                    </div>
+                                    <div className='flex items-center'>
+                                        <button className="bg-orange-400 rounded-full px-4 py-2 text-white">
+                                            Detalles
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+
+                        ))}
+                    </tbody>
+                </table>
 
 
-            <div className="mt-4 flex justify-center items-center">
-                <button
-                    className="bg-black rounded-full px-4 py-2 text-white"
-                    onClick={handleClickPrevPage}
-                >
-                    &#9666;
-                </button>
-                <div className="flex items-center text-white">
+                <div className="mt-4 flex justify-center items-center text-white">
+                    <button className="bg-black text-white p-1" onClick={handleClickPrevPage}>&#9666;</button>
+
                     {Array.from({ length: totalPages }, (_, index) => (
                         <button
                             key={index}
-                            className={`mx-2 ${currentPage === index + 1
-                                    ? 'bg-orange-500'
-                                    : 'bg-black'
-                                } rounded-full px-4 py-2 text-white`}
+                            className={`mx-1 ${currentPage === index + 1 ? 'bg-orange-400' : 'bg-black'} rounded-full px-3 py-1`}
                             onClick={() => handlePageClick(index + 1)}
                         >
                             {index + 1}
                         </button>
                     ))}
+
+                    <button className="bg-black rounded-full p-1" onClick={handleClickNextPage}>
+                        &#9656;
+                        </button>
                 </div>
-                <button
-                    className="bg-black rounded-full px-4 py-2 text-white"
-                    onClick={handleClickNextPage}
-                >
-                    &#9656;
-                </button>
+
+
+
+
             </div>
-
-
-        </div>
         </>
 
     );
