01974eaee58a1f3e05b439033a9485c931212aad
jose22gonzalez <urenajosealbeto@gmail.com>
2024-01-24 09:13:04 -0400

    PSP-76

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -6,27 +6,30 @@ import { FaArrowRight } from 'react-icons/fa';
 const Bar = () => {
     return (
         <div className=''>
-            <div className='bg-[#2c2c2c] w-auto flex items-center justify-center rounded-full'>
-                <div className='grid grid-cols-4 items-center p-2'>
+            <div className='bg-[#2c2c2c] w-auto  flex items-center justify-center rounded-full'>
+                <div className='grid grid-cols-3 items-center p-2'>
                     <div className='flex flex-col mx-6'>
                         <span className='text-white'>User information</span>
                         <span className='text-gray-500'>Manage your account details</span>
                     </div>
-                    <div className='border-l pl-4'>
+                    <div className='border-l border-gray-700 pl-4'>
                         <div className='flex flex-col'>
                             <span className='text-white'>Notifications</span>
                             <span className='text-gray-500'>View important updates</span>
                         </div>
                     </div>
-                    <div className='border-l pl-4 flex flex-col mx-6'>
-                       
-                        <span className='text-white'>Airplane Rentals</span>
-                        <span className='text-gray-500'>Rent a Plane to fly</span>
-                    </div>
-                    <div className=' pl-4'>
+                    <div className='border-l border-gray-700 pl-4 flex flex-row mx-6'>
+                        <div className='flex flex-col'>
+                            <span className='text-white'>Airplane Rentals</span>
+                            <span className='text-gray-500'>Rent a Plane to fly</span>
+                        </div>
+
+
+                       <div className='ml-[2rem]'>
                         <button className='bg-orange-500 rounded-full p-4'>
                             <FaArrowRight />
                         </button>
+                    </div>
                     </div>
                 </div>
             </div>
--- src/components/CardExplore/CardExplore.jsx
+++ src/components/CardExplore/CardExplore.jsx
@@ -1,41 +1,31 @@
-// src/components/CardExplore.js
-
 import React from 'react';
 import aircraft from '../../assets/aircraft.jpg';
-import Bar from '../Bar/Bar'
+import Bar from '../Bar/Bar';
 
 const CardExplore = () => {
     return (
-        <div className=''>
-            <div className="relative">
-                <div
-                    className="bg-cover bg-center text-white text-center relative rounded-xl"
-                    style={{
-                        backgroundImage: `url(${aircraft})`,
-                        height: '300px',
-                    }}
-                >
-                    <div className="absolute inset-0 bg-black opacity-50"></div>
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <div>
-                            <h1 className="text-4xl font-bold mb-4">
-                                Explore Our Airplane Rental Services
-                            </h1>
-                            <p className="text-lg">
-                                Choose from a wide range of airplanes for your next flight!
-                            </p>
-                        </div>
-                    </div>
+        <div className='relative'>
+            <div
+                className="bg-cover bg-center text-white text-center relative rounded-xl"
+                style={{
+                    backgroundImage: `url(${aircraft})`,
+                    height: '300px',
+                }}
+            >
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 flex items-center justify-center flex-col">
+                    <h1 className="text-4xl font-bold mb-4">
+                        Explore Our Airplane Rental Services
+                    </h1>
+                    <p className="text-lg">
+                        Choose from a wide range of airplanes for your next flight!
+                    </p>
+
                 </div>
-
-
-
-
             </div>
             <div className='flex justify-center'>
                 <Bar />
             </div>
-
         </div>
     );
 };
