9a2173c997a12b41b69c454f07b5156114daf7b3
Sander Peguero <95009440+SanderPeguero@users.noreply.github.com>
2024-01-24 05:59:07 -0400

    Revert "PSP-28"

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,11 +1,10 @@
 import { createContext, useContext, useState, useEffect } from "react"
-import { db,auth } from "./firebase/firebase"
-import { set, ref, onValue, get, update, push,getDatabase } from "firebase/database"
-import { signInWithEmailAndPassword,createUserWithEmailAndPassword, signOut, updateProfile } from "firebase/auth"
-import { toast } from "react-toastify"
+import { db } from "./firebase/firebase"
+import { set, ref,query, orderByChild, equalTo, onValue, get, update, push, remove } from "firebase/database"
+
 
 const Context = createContext()
-//123456789
+
 export const useContextAir = () => {
     const context  = useContext(Context)
     if(!context) throw new Error('There is no Context provider')
@@ -112,50 +111,8 @@ export function ProviderContext({children}) {
         console.log(AllReservations)
       }, [ReservationsForDate, AllReservations])
       
-      async function saveUserDetails(email, userDetails) {
-        const db = getDatabase();
-        const userRef = ref(db, 'users/' + email.replace('.', ',')); // Usando el correo electrónico como clave, reemplazando '.' con ','
       
-        try {
-          await set(userRef, userDetails);
-          console.log('Detalles del usuario guardados correctamente en la base de datos');
-        } catch (error) {
-          console.error('Error al guardar detalles del usuario:', error.message);
-        }
-      }
-      
-      const signup = async (email, password,name,phonenumber) => {
-        // Validar el formato del correo electrónico
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-          console.error("Error: El formato del correo electrónico no es válido");
-          return;
-        }
-      
-        try {
-          await createUserWithEmailAndPassword(auth, email, password);
-          console.log("Usuario registrado exitosamente");
-          updateProfile(
-            auth.currentUser,
-            { displayName: name  }
-          )
-          // Guardar detalles adicionales del usuario en la base de datos
-          await saveUserDetails(email, { name, phonenumber });
-        
-        } catch (error) {
-          // Manejar errores específicos
-          if (error.code === "auth/weak-password") {
-            console.error("Error: La contraseña es débil. Debe tener al menos 6 caracteres");
-          } else if (error.code === "auth/email-already-in-use") {
-            console.error("Error: El correo electrónico ya está en uso. Prueba con otro");
-          } else if (error.code === "auth/invalid-email") {
-            console.error("Error: El formato del correo electrónico no es válido");
-          } else {
-            console.error("Error al registrar usuario:", error.message);
-          }
-        
-        }
-      }
+
 
       
     return (
@@ -163,9 +120,10 @@ export function ProviderContext({children}) {
             value={{
                 ShowListHours,
                 ReservationsForDate,
+                AllReservations,
                 SaveScheduledform,
-                signup,
-                useContextAir
+                DeleteScheduleById,
+                EditScheduleById
             }}
         >
             {children}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database"
-import {getAuth} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getDatabase()
-const auth = getAuth(app)
-export { db,auth }
+
+export { db }
