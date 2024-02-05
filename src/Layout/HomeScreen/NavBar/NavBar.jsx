994db43439520c2c40a4a5c628d101aa7906650a
jose22gonzalez <urenajosealbeto@gmail.com>
2024-02-05 09:35:03 -0400

    PSP-84 Log In-Sign-Up

--- src/Layout/HomeScreen/NavBar/NavBar.jsx
+++ src/Layout/HomeScreen/NavBar/NavBar.jsx
@@ -19,8 +19,8 @@ const NavBar = () => {
     <nav className="bg-[#2c2c2c] p-4 flex justify-between items-center">
       <div className="text-white text-xl font-bold">Airplane Rent</div>
       <div className="hidden md:flex items-center space-x-4">
-        <Link to='/Signln' className="bg-white text-black px-4 py-2 rounded-full hidden md:inline">Sign up</Link>
-        <Link to="/LogIn" className="bg-orange-500 text-white px-4 py-2 rounded-full hidden md:inline">Log In</Link>
+        <Link to="/LogIn" className="bg-white text-black px-4 py-2 rounded-full hidden md:inline">Log In</Link>
+        <Link to='/Signln' className="bg-orange-500 text-white px-4 py-2 rounded-full hidden md:inline">Sign Up</Link>
       </div>
       <div className="md:hidden">
         <button
@@ -31,7 +31,7 @@ const NavBar = () => {
         </button>
         {isMobileMenuOpen && (
           <div className="absolute top-16 left-0 right-0 bg-[#2c2c2c] py-2 flex flex-row items-center  z-50">
-           <Link to='/Signln' className="block text-black px-4 py-2 p-2 m-2 bg-white rounded-full">Sign up</Link>
+            <Link to='/Signln' className="block text-black px-4 py-2 p-2 m-2 bg-white rounded-full">Sign up</Link>
             <Link to="/LogIn" className="block text-white px-4 py-2 p-2 m-2 bg-orange-500 rounded-full">Log In</Link>
           </div>
         )}
