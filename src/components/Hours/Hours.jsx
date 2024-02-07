9195eb3abddae7ebef5610729459dca9b4ffac80
jose22gonzalez <urenajosealbeto@gmail.com>
2024-02-07 16:04:10 -0400

    PSP-96 Modal Calendar and List Hours

--- src/components/Hours/Hours.jsx
+++ src/components/Hours/Hours.jsx
@@ -5,7 +5,8 @@ import { format } from 'date-fns';
 import { useContextAir } from '../../Context';
 import { FaRegCalendarAlt } from "react-icons/fa";
 import iconCalendar from "../../Icon/icon_cal.png"
-function Hours() {
+import { IoMdClose } from "react-icons/io";
+function Hours({ openHourModal, setOpenHourModal,setIsOpenCalander, DateH, monthH, yearH }) {
   const datos = useParams();
   const {
     ShowListHours,
@@ -18,9 +19,12 @@ function Hours() {
   } = useContextAir()
   // const fechaEspecifica = new Date()
   const [fechaEspecifica, setFechaEspecifica] = useState(new Date());
-  fechaEspecifica.setDate(datos.Dia)
-  fechaEspecifica.setFullYear(datos.Year)
-  fechaEspecifica.setMonth(datos.Month)
+  // fechaEspecifica.setDate(datos.Dia)
+  // fechaEspecifica.setFullYear(datos.Year)
+  // fechaEspecifica.setMonth(datos.Month)
+  fechaEspecifica.setDate(DateH)
+  fechaEspecifica.setFullYear(yearH)
+  fechaEspecifica.setMonth(monthH)
   const [horasDelDia, setHorasDelDia] = useState([]);
 
   const [reservations, setReservations] = useState([]);
@@ -108,163 +112,180 @@ function Hours() {
 
 
 
+  const handleOpenModalCalander = (e) => {
+    e.preventDefault()
+    console.log("Open calander")
+    setIsOpenCalander(true)
+    setOpenHourModal(false)
+  }
 
-
-
-
-
+  const handleCloseModalHour = (e) => {
+    e.preventDefault()
+    setOpenHourModal(false)
+    setIsOpenCalander(false)
+  }
 
   return (
-    <div className="bg-[#070d16] text-white">
+    <div>
+      {openHourModal && (
+        <div className='fixed inset-0 overflow-auto'>
+          <div className="bg-[#070d16] text-white">
 
-      <ScheduleForm isOpen={isOpen} setIsOpen={setIsOpen} onSave={handleSaveModalData} reservations={reservations} setReservations={setReservations} date={mesActual} reservationEdit={reservationEdit} setreservationEdit={setreservationEdit} />
+            <ScheduleForm isOpen={isOpen} setIsOpen={setIsOpen} onSave={handleSaveModalData} reservations={reservations} setReservations={setReservations} date={mesActual} reservationEdit={reservationEdit} setreservationEdit={setreservationEdit} />
 
-      <div className=' w-full h-[5rem] flex items-center border-b-2'>
-        <div className='flex items-center justify-between py-2 px-6'>
-          <div className='px-1 flex items-center'>
+            <div className=' w-full h-[5rem] flex items-center border-b-2'>
+              <div className='flex items-center justify-between py-2 px-6 w-full'>
+                <div className='px-1 flex items-center'>
 
-            <div className='flex row '>
-              <Link className="pl-4 pr-4" to={"/"}>
-                {/* < FaRegCalendarAlt size={30} /> */}
-                {/* <img src={iconCalendar} className='w-16 h-16 filter brightness-0 invert saturate-150 contrast-200' /> */}
-                <img src={iconCalendar} className='w-16 h-16' />
+                  <div className='flex row '>
 
-              </Link>
+                    <button onClick={(e) => handleOpenModalCalander(e)} className="pl-4 pr-4" to={"/"}>
+                      <img src={iconCalendar} className='w-16 h-16' />
 
-
-
-              <button
-                type='button'
-                className='leading-none rounded-full transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center'
-                onClick={irAlMesAnterior}
-              >
-                <svg
-                  className='h-6 w-6 text-gray-500 inline-flex leading-none'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                >
-                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15 19l-7-7 7-7'></path>
-                </svg>
-              </button>
-              <div className=' inline-flex h-6'></div>
-              <button
-                type='button'
-                className='leading-none rounded-full transition ease-in-out duration-100 inline-flex items-center cursor-pointer hover:bg-gray-200 p-1'
-                onClick={irAlMesSiguiente}
-              >
-                <svg
-                  className='h-6 w-6 text-gray-500 inline-flex leading-none'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
-                >
-                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M9 5l7 7-7 7'></path>
-                </svg>
-              </button>
+                    </button>
 
 
 
-            </div>
-            <div className='ml-4 text-white'>
-              <span className="text-lg  font-normal">{mesActual.toLocaleString('default', { day: 'numeric' }) + " de "}</span>
-              <span className="text-lg   mr-1">{mesActual.toLocaleString('default', { month: 'long' })}</span>
-              <span className="text-lg  font-normal">{mesActual.toLocaleString('default', { year: 'numeric' })}</span>
-            </div>
-          </div>
-        </div>
-      </div>
+                    <button
+                      type='button'
+                      className='leading-none rounded-full transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center'
+                      onClick={irAlMesAnterior}
+                    >
+                      <svg
+                        className='h-6 w-6 text-gray-500 inline-flex leading-none'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15 19l-7-7 7-7'></path>
+                      </svg>
+                    </button>
+                    <div className=' inline-flex h-6'></div>
+                    <button
+                      type='button'
+                      className='leading-none rounded-full transition ease-in-out duration-100 inline-flex items-center cursor-pointer hover:bg-gray-200 p-1'
+                      onClick={irAlMesSiguiente}
+                    >
+                      <svg
+                        className='h-6 w-6 text-gray-500 inline-flex leading-none'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                      >
+                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M9 5l7 7-7 7'></path>
+                      </svg>
+                    </button>
 
 
-      <div className="grid grid-rows-8 pl-10 mx-4">
-        {/* gap-4 */}
-        {/* `bg-${Color}` */}
-        {/*  */}
-        <div className="pt-4">Hora</div>
-        {horasDelDia.map((hora, index) => {
-          const reservacionesEnEstaHora = reservationsA.filter(
-            reserva =>
-              reserva.start <= hora && reserva.end > hora &&
-              formattedMesActual === reserva.date
-          );
 
-          const isHoraReservada = reservacionesEnEstaHora.length > 0;
-          let reservaMostrar = null;
-
-          if (isHoraReservada) {
-            reservaMostrar = reservacionesEnEstaHora.find(reserva =>
-              reserva.start <= hora && reserva.end >= hora
-            );
-          }
-
-          return (
-            <div key={index} className='flex h-[2rem]  sm:h-[5rem]'>
-              <span className={`mr-2 ${hora <= 9 ? 'pr-10' : 'pr-8'}`}>
-                {hora}
-              </span>
-              <div
-                key={index}
-                className={` ${isHoraReservada ? 'flex-1  p-6 bg-[#0d7ca8]' : 'flex-1 border p-6 cursor-pointer  hover:bg-gray-200  transition ease-in-out'
-                  }`}
-                onClick={() => {
-                  if (reservaMostrar) {
-                    if (CanEdit === true) {
-                      if (WhichRole === "user") {
-                        if (user && user.uid === reservaMostrar.id_user) {
-                          setIsOpen(true);
-                          setreservationEdit(reservaMostrar)
-                        }
-                      } else {
-                        setIsOpen(true);
-                        setreservationEdit(reservaMostrar)
-                      }
-
-                    } else {
-                      window.confirm('You do not have permission to edit or delete!');
-                    }
-
-
-                  } else {
-                    if (CanReservation === true) {
-                      setIsOpen(true);
-                    } else {
-                      window.confirm('You do not have permission to reserve!');
-                    }
-
-                  }
-
-                }}
-              >
-                {reservacionesEnEstaHora.map((reserva, reservaIndex) => (
-                  <div key={reservaIndex} className=''>
-                    {
-                      formattedMesActual === reserva.date && (
-                        reserva.start <= hora && reserva.end >= hora && (
-                          reserva.start === hora && (
-                            <div key={reservaIndex} className='text-xs text-white '>
-                              <p className='font-bold'>{reserva.name}</p>
-                              <p>{reserva.start >= "13:00" ? reserva.start + " pm" : reserva.start + " am"} - {reserva.end >= "13:00" ? reserva.end + " pm" : reserva.end + " am"}</p> <p>{reserva.fuel} Gal</p>
-
-                            </div>
-                          )
-                        )
-                      )
-                    }
                   </div>
-
-                )
-                )}
-
+                  <div className='ml-4 text-white'>
+                    <span className="text-lg  font-normal">{mesActual.toLocaleString('default', { day: 'numeric' }) + " de "}</span>
+                    <span className="text-lg   mr-1">{mesActual.toLocaleString('default', { month: 'long' })}</span>
+                    <span className="text-lg  font-normal">{mesActual.toLocaleString('default', { year: 'numeric' })}</span>
+                  </div>
+                </div>
+                <button onClick={(e) => handleCloseModalHour(e)} className='hover:bg-red-500 p-2 rounded-full'>
+                  <IoMdClose size={25} color='white' />
+                </button>
               </div>
             </div>
-          );
-        })}
+
+
+            <div className="grid grid-rows-8 pl-10 mx-4">
+              {/* gap-4 */}
+              {/* `bg-${Color}` */}
+              {/*  */}
+              <div className="pt-4">Hora</div>
+              {horasDelDia.map((hora, index) => {
+                const reservacionesEnEstaHora = reservationsA.filter(
+                  reserva =>
+                    reserva.start <= hora && reserva.end > hora &&
+                    formattedMesActual === reserva.date
+                );
+
+                const isHoraReservada = reservacionesEnEstaHora.length > 0;
+                let reservaMostrar = null;
+
+                if (isHoraReservada) {
+                  reservaMostrar = reservacionesEnEstaHora.find(reserva =>
+                    reserva.start <= hora && reserva.end >= hora
+                  );
+                }
+
+                return (
+                  <div key={index} className='flex h-[2rem]  sm:h-[5rem]'>
+                    <span className={`mr-2 ${hora <= 9 ? 'pr-10' : 'pr-8'}`}>
+                      {hora}
+                    </span>
+                    <div
+                      key={index}
+                      className={` ${isHoraReservada ? 'flex-1  p-6 bg-[#0d7ca8]' : 'flex-1 border p-6 cursor-pointer  hover:bg-gray-200  transition ease-in-out'
+                        }`}
+                      onClick={() => {
+                        if (reservaMostrar) {
+                          if (CanEdit === true) {
+                            if (WhichRole === "user") {
+                              if (user && user.uid === reservaMostrar.id_user) {
+                                setIsOpen(true);
+                                setreservationEdit(reservaMostrar)
+                              }
+                            } else {
+                              setIsOpen(true);
+                              setreservationEdit(reservaMostrar)
+                            }
+
+                          } else {
+                            window.confirm('You do not have permission to edit or delete!');
+                          }
+
+
+                        } else {
+                          if (CanReservation === true) {
+                            setIsOpen(true);
+                          } else {
+                            window.confirm('You do not have permission to reserve!');
+                          }
+
+                        }
+
+                      }}
+                    >
+                      {reservacionesEnEstaHora.map((reserva, reservaIndex) => (
+                        <div key={reservaIndex} className=''>
+                          {
+                            formattedMesActual === reserva.date && (
+                              reserva.start <= hora && reserva.end >= hora && (
+                                reserva.start === hora && (
+                                  <div key={reservaIndex} className='text-xs text-white '>
+                                    <p className='font-bold'>{reserva.name}</p>
+                                    <p>{reserva.start >= "13:00" ? reserva.start + " pm" : reserva.start + " am"} - {reserva.end >= "13:00" ? reserva.end + " pm" : reserva.end + " am"}</p> <p>{reserva.fuel} Gal</p>
+
+                                  </div>
+                                )
+                              )
+                            )
+                          }
+                        </div>
+
+                      )
+                      )}
+
+                    </div>
+                  </div>
+                );
+              })}
 
 
 
-      </div>
+            </div>
+
+          </div>
+        </div>
+      )}
 
     </div>
+
   );
 }
 
