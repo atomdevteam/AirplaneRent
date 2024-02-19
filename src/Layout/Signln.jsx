75ad040742629f7a729dc31539d203e4f9836bff
jose22gonzalez <urenajosealbeto@gmail.com>
2024-02-19 09:36:53 -0400

    PSP-85 Code Cleaned

--- src/Layout/Signln.jsx
+++ src/Layout/Signln.jsx
@@ -12,15 +12,6 @@ const Signln = ({ isOpen, setIsOpen, setIsLogIn }) => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const { signup } = useContextAir();
-    // const handleEmailChange = (e) => {
-    //     setEmail(e.target.value);
-    //     console.log(email)
-    // };
-
-    // const handlePasswordChange = (e) => {
-    //     setPassword(e.target.value);
-    //     console.log(password)
-    // };
 
     const signInHandler = (e) => {
         e.preventDefault();
@@ -33,9 +24,7 @@ const Signln = ({ isOpen, setIsOpen, setIsLogIn }) => {
                         email: email,
                         password: password
                     }
-
                     signup(datos)
-                    // history('/', { replace: true });
                 } else {
                     toast.error("Passwords Are Not Equal",
                         {
@@ -165,91 +154,7 @@ const Signln = ({ isOpen, setIsOpen, setIsLogIn }) => {
 
                         </div>
                     </div>
-
-                    {/* <div className='bg-white rounded-lg p-6 w-96 max-w-full shadow-lg transform transition-all duration-300 '>
-                        <div className='flex justify-between items-center border-b-2 border-gray-200 pb-4'>
-                            <h2 className="text-2xl font-semibold">Sign in with</h2>
-                            <button
-                                onClick={(e) => handleCloseModal(e)}
-                                className='text-gray-500 hover:text-gray-700 focus:outline-none'
-                            >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x">
-                                    <line x1="18" y1="6" x2="6" y2="18"></line>
-                                    <line x1="6" y1="6" x2="18" y2="18"></line>
-                                </svg>
-                            </button>
-                        </div>
-
-                        <div className='mt-6 space-y-4'>
-
-                            <div className="">
-                                <label className="font-semibold text-sm text-gray-600 pb-1 block">NAME</label>
-                                <input
-                                    type="text"
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                    className="border px-3 py-2 mt-1 mb-5 rounded-md text-sm w-full  shadow-md"
-                                />
-
-
-                                <label className="font-semibold text-sm text-gray-600 pb-1 block">PHONE NUMBER</label>
-                                <input
-                                    type="text"
-                                    value={phonenumber}
-                                    onChange={(e) => setPhonenumber(e.target.value)}
-                                    className="border px-3 py-2 mt-1 mb-5 text-sm w-full  shadow-md"
-                                />
-                                <label className="font-semibold text-sm rounded-md text-gray-600 pb-1 block ">EMAIL</label>
-                                <input
-                                    type="text"
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    className="border px-3 py-2 mt-1 rounded-md mb-5 text-sm w-full  shadow-md"
-                                />
-                                <label className="font-semibold text-sm text-gray-600 pb-1 block">PASSWORD</label>
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    className="border px-3 py-2 mt-1 rounded-md mb-5 text-sm w-full  shadow-md"
-                                />
-                                <label className="font-semibold text-sm text-gray-600 pb-1 block">CONFIRM PASSWORD</label>
-                                <input
-                                    type="password"
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                    className="border px-3 py-2 mt-1 mb-5 text-sm w-full  rounded-md shadow-md"
-                                />
-                                <button
-                                    type="button"
-                                    className="transition duration-200 bg-blue-700 hover:bg-blue-500 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-md text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
-                                    onClick={signInHandler}
-                                >
-                                    <span className="inline-block mr-2">Signln</span>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        className="w-4 h-4 inline-block"
-                                    >
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                    </svg>
-                                </button>
-                            </div>
-
-                        </div>
-
-                        <div className="my-4">
-
-                            <div className="text-center text-xs">
-                                Do you have an account?<button onClick={(e) => handleOpenLogIn(e)} className=" hover:underline transition-all underline ">Log in</button>
-                            </div>
-                        </div>
-
-                    </div> */}
                 </div>
-
             }
         </div>
 
